4f2d44eb34b1ac1d5dbb6b79a91a1552c2d49198
Jiancodes <mjian528@gmail.com>
2023-09-28 21:09:55 +0800

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,20 @@
+//
+// Created by mjdev on 2023/9/28.
+//
+#include <iostream>
+int main(){
+
+    //This is a comment
+
+    /*
+        This
+        is
+        a
+        multi-line
+        comment
+     */
+
+    std::cout << "I like pizza!" << std::endl;
+    std::cout << "It's really good!" << std::endl;
+    return 0;
+}
